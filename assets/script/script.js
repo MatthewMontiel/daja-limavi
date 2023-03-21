b176dcea22a9ad6054ad4edb4577d52385e5ddb6
vrich88 <mvrichards@icloud.com>
2023-03-20 19:37:07 -0500

    update on button targeting

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,45 +1,17 @@
-// function to highlight genre modifier
-// let activeGM = 'container-active'; // define a css class 'constant'
-
-// let el = document.getElementById('useGM');
-// el.addEventListener('click', function () {
-//     let classes,
-//         idx;
-
-//     // simplest case: class name is empty
-//     if (this.className === '') {
-//         this.className = activeGM;
-//         return;
-//     }
-
-//     // next: class name matches active
-//     if (this.className === activeGM) {
-//         this.className = '';
-//         return;
-//     }
-
-//     // otherwise, if more complex, parse and modify classes
-//     classes = this.className.split(' ');
-//     idx = classes.indexOf(activeGM);
-//     if (idx === -1) {
-//         classes.push(activeGM);
-//     } else {
-//         classes.splice(idx, 1);
-//     }
-//     this.className = classes.join(' ');
-// });
-
-
-// to store the clicked on genre modifiers as an arrays
-activeGMods = [];
+let activeGMods = [];
 GenModTray = document.querySelector("#gmTray");
 GMods = document.querySelectorAll("[data-genMod]");
 
-GenModTray.addeventlistener("click", addGenMod());
+// this part works but isn't the desired end result
+$(GenModTray).click(function() {
+    activeGMods.push(this).innerHTML;console.log(activeGMods);
+    });
 
-function addGenMod() {
-    GMods = document.querySelectorAll("[data-genMod]"); console.log(GMods);
-    console.log("click test on");
-    $(this).children().attr("[data-genMod]");
-    activeGMods = JSON.parse(this);
-}
+// $(GenModTray).click(function() {
+//     if (EventTarget.value === "inactive") {
+//         EventTarget.setAttribute("value","active");
+//     } else {
+//         EventTarget.set("value","inactive");
+//     }
+//     console.log(activeGMods);
+// });
