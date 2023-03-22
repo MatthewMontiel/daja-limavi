40866f8017611b6f075f75d428228fc401a90865
vrich88 <mvrichards@icloud.com>
2023-03-21 21:01:20 -0500

    function inprovments

---
+++ assets/script/mainfetch.js
@@ -0,0 +1,9 @@
+// import variables 
+    import activeGMods from "./script";
+// global variables
+    let fetchURL = 1;
+    let results = "a";
+// functions area
+
+// export variables 
+    export default results
