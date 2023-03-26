07b29f7a7d59b7db6e94631005c655d522766017
vrich88 <mvrichards@icloud.com>
2023-03-25 21:31:51 -0500

    file clean up and stylizations

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,38 +1,49 @@
 // import variables 
 // import results from "./mainfetch";
 // global variables
-  let userSearch = document.querySelector("#searchTerm");
-  let resultsListing = document.querySelector("#resultsListing");
-  let activeGMods = [];
-  let genModTray = document.querySelector("#gmTray");
-  let gMods = document.querySelectorAll("[data-genMod]");
-  // console.log(gMods);
+let userSearch = document.querySelector("#searchTerm");
+let resultsListing = document.querySelector("#resultsListing");
+let activeGMods = [];
+let genModTray = document.querySelector("#gmTray");
+let gMods = document.querySelectorAll("[data-genMod]");
+let loadDeckBtn = document.querySelector("#search");
+let shuffleBtn = document.querySelector("#log");
+let displayGenres = document.querySelector("#displayGenres");
 
 // functions area
-  // primary function cased on genre modifier click selection will add/remove
-  $(genModTray).click(function (event) {
-    let gmodState = event.target.getAttribute("value");
-    let gmodString = event.target.getAttribute("title");
-    if (gmodState === "inactive") {
-      event.target.setAttribute("value", "active");
-      // console.log(gmodState);
-      activeGMods.push(gmodString);
-      // console.log(gmodString);
-    } else {
-      event.target.setAttribute("value", "inactive");
-      console.log(gmodState);
-      function removeGMod() {
-        let gmodIndex = activeGMods.indexOf(gmodString); console.log(gmodIndex);
-        activeGMods.splice(gmodIndex,1);
-      }
-      removeGMod();
-      // console.log(gmodString);
+// primary function cased on genre modifier click selection will add/remove
+$(genModTray).click(function (event) {
+  let gmodState = event.target.getAttribute("value");
+  let gmodString = event.target.getAttribute("title");
+  if (gmodState === "inactive") {
+    event.target.setAttribute("value", "active");
+    // console.log(gmodState);
+    activeGMods.push(gmodString);
+    // console.log(gmodString);
+  } else {
+    event.target.setAttribute("value", "inactive");
+    // console.log(gmodState);
+    function removeGMod() {
+      let gmodIndex = activeGMods.indexOf(gmodString); console.log(gmodIndex);
+      activeGMods.splice(gmodIndex, 1);
     }
-    console.log(activeGMods);
-  });
-  // function to write results in display list
-  // // function displayResults () {
-    
-  // };
+    removeGMod();
+    // console.log(gmodString);
+  }
+  console.log(activeGMods);
+});
+// function to switch Load Deck & Shuffle buttons and write activeGMods to search results Tablet
+$(loadDeckBtn).click(function (event) {
+  loadDeckBtn.classList.toggle("hide");
+  shuffleBtn.classList.toggle("hide");
+  displayGenres.innerHTML = activeGMods[0] + " + " + activeGMods[1] + " + " + activeGMods[2];
+});
+// function to switch Load Deck & Shuffle buttons and clear activeGMods to search results Tablet
+$(shuffleBtn).click(function (event) {
+  loadDeckBtn.classList.toggle("hide");
+  shuffleBtn.classList.toggle("hide");
+});
+
+
 // export variables 
-  export default activeGMods;
+export default activeGMods;
